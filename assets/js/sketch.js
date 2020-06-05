c03dfa7fffec437087a27b90398e1dd99de81a32
Yuriy Shiryaev <yuriysteam@icloud.com>
2020-06-05 22:34:12 +0200

    font jiggle

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,25 +1,21 @@
 var div, start_button, mic, micEnabled
 
 function setup() {
-  noCanvas()
-  div = document.getElementById('arttext')
-// document.getElementById('funstarts').mousePressed(userStartAudio);
+  noCanvas();
+  div = document.getElementById('arttext');
     
-    
-   // start_button = document.getElementById('funstarts')
- //   start_button.mousePressed(userStartAudio);
-    mic = new p5.AudioIn()
+    mic = new p5.AudioIn();
 }
 
 function toggleMic() {
-    mic.start()
-  micEnabled = true
+    mic.start();
+    micEnabled = true;
 }
 
 
 function draw() {
-  micLevel = mic.getLevel()
+  micLevel = mic.getLevel();
   // weightControl = sin(frameCount/1.0)*100;
-  div.style['font-variation-settings'] = `"wght" ${micLevel * 4000}, "wdth" ${micLevel * 5000}`
-  console.log(micLevel)
+  div.style['font-variation-settings'] = `"wght" ${micLevel * 4000}, "wdth" ${micLevel * 5000}`;
+  console.log(micLevel);
 }
